749f5b9c4f803fa900e835b46252479e0fa81eb9
ThaisDavilla <thaisdavilla@yahoo.com.br>
2023-04-22 19:53:40 -0300

    Estruturação mangoose. Testes com o Insonmia.

--- bancoDeDados.js
+++ bancoDeDados.js
@@ -1,12 +1,13 @@
 const mongoose = require('mongoose')
+require('dotenv').config()
 
 async function conectaBancoDeDados(){
     try{
         console.log('Conexão com o banco de dados iniciou')
 
-    await mongoose.conect('mongodb+srv://thaisdavilla:rxoEWbsf6rwI1Po@clustermulheres.ln3naqp.mongodb.net/?retryWrites=true&w=majority')
+        await mongoose.connect(process.env.MONGO_URL)
 
-    console.log('Conexão com o banco de dados feita com sucesso.')
+        console.log('Conexão com o banco de dados feita com sucesso.')
     } catch(erro){
         console.log(erro)
     }
--- mulheres.js
+++ mulheres.js
@@ -1,92 +1,89 @@
 const express = require("express") // iniciando o express
 const router = express.Router() // configurando a primeira parte da rota
-const { v4: uuidv4 } = require('uuid');
+
+const cors = require('cors')// permissão para consumo da api
 
 const conectaBancoDeDados = require('./bancoDeDados') //ligando ao arquivo banco de dados
 conectaBancoDeDados() //chamando a função que liga ao banco de dados
 
+const Mulher = require('./mulherModel')
+
 const app = express() //iniciando o app
 app.use(express.json())
+app.use(cors())
+
 const porta = 3333 //criando a porta
 
-//lista inicial de mulheres
-const mulheres = [
-    {
-        id: '1',
-        nome: "Thais D'Ávilla",
-        imagem: "https://media.licdn.com/dms/image/D4D03AQH6mZowXOLSCQ/profile-displayphoto-shrink_200_200/0/1668211988889?e=1686787200&v=beta&t=kwtVIRlUSr3vpysSZmvI5fZ7fSa7uVdN84YwYsI5n_4",
-        minibio: "Front End Developer"
-    },
-    {
-        id:'2',
-        nome: "Thais D'Ávilla",
-        imagem: "https://media.licdn.com/dms/image/D4D03AQH6mZowXOLSCQ/profile-displayphoto-shrink_200_200/0/1668211988889?e=1686787200&v=beta&t=kwtVIRlUSr3vpysSZmvI5fZ7fSa7uVdN84YwYsI5n_4",
-        minibio: "Front End Developer"
-    },
-    {
-        id:'3',
-        nome: "Thais D'Ávilla",
-        imagem: "https://media.licdn.com/dms/image/D4D03AQH6mZowXOLSCQ/profile-displayphoto-shrink_200_200/0/1668211988889?e=1686787200&v=beta&t=kwtVIRlUSr3vpysSZmvI5fZ7fSa7uVdN84YwYsI5n_4",
-        minibio: "Front End Developer"
-    }
-]
-
 //get
-function mostraMulheres(request, response) {
-    response.json(mulheres)
+async function mostraMulheres(request, response) {
+    try{
+        const mulheresVindasDoBancoDeDados = await Mulher.find()
+
+        response.json(mulheresVindasDoBancoDeDados)
+
+    }catch (erro){
+        console.log(erro)
+    }
+    
 }
 
 //post
-function criaMulher(request, response){
-    const novaMulher = {
-        id: uuidv4(),
+async function criaMulher(request, response){
+    const novaMulher = new Mulher ({
         nome: request.body.nome,
         imagem: request.body.imagem,
-        minibio: request.body.minibio
+        minibio: request.body.minibio,
+        citacao: request.body.citacao
+    })
+
+    try {
+        const mulherCriada = await novaMulher.save()
+        response.status(201).json(mulherCriada)
+    }catch(erro){
+        console.log(erro)
     }
-
-    mulheres.push(novaMulher)
-
-    response.json(mulheres)
 }
 
 //patch
-function corrigeMulher(request, response){
-    function encontraMulher(mulher) {
-        if (mulher.id === request.params.id){
-            return mulher 
+async function corrigeMulher(request, response){
+
+    try{
+        const mulherEncontrada = await Mulher.findById(request.params.id)
+
+        if (request.body.nome){
+            mulherEncontrada.nome = request.body.nome
+        }
+    
+        if (request.body.minibio){
+            mulherEncontrada.minibio = request.body.minibio
+        }
+    
+        if (request.body.imagem){
+            mulherEncontrada = request.body.imagem
         }
 
+        if (request.body.citacao){
+            mulherEncontrada = request.body.citacao
+        }
+
+        const mulherAtualizadaNoBancoDeDados = await mulherEncontrada.save()
+        response.json(mulherAtualizadaNoBancoDeDados)
+
+    }catch(erro){
+        console.log(erro)
     }
-
-    const mulherEncontrada = mulheres.find(encontraMulher)
-
-    if (request.body.nome){
-        mulherEncontrada.nome = request.body.nome
-    }
-
-    if (request.body.minibio){
-        mulherEncontrada.minibio = request.body.minibio
-    }
-
-    if (request.body.imagem){
-        mulherEncontrada.imagem = request.body.imagem
-    }
-
-    response.json(mulheres)
 }
 
 //delete
-function deletaMulher(request, response){
-    function todasMenosEla(mulher) {
-        if(mulher.id !== request.params.id) {
-            return mulher
-        }
+async function deletaMulher(request, response){
+    try{
+        await Mulher.findByIdAndDelete(request.params.id)
+        response.json({mensagem: 'Mulher deletada com sucesso!'})
+        
+    }catch(erro){
+        console.log(erro)
     }
 
-    const mulheresQueFicam = mulheres.filter(todasMenosEla)
-
-    response.json(mulheresQueFicam)
 }
 
 app.use(router.get("/mulheres", mostraMulheres)) //rota get/mulheres
